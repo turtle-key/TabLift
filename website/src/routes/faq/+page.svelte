ebcbde290b66c4eca46046a932f9a9361818d24a
Mihai Ghețu <mihaieduardghetu@yahoo.com>
2025-06-24 17:51:22 +0300

    Added support for dark mode on privacy-policy and faq pages

--- website/src/routes/faq/+page.svelte
+++ website/src/routes/faq/+page.svelte
@@ -3,7 +3,7 @@
   <meta name="description" content="Find answers to frequently asked questions about TabLift, the macOS utility that makes switching between apps with ⌘+Tab more intuitive by including minimized windows." />
 </svelte:head>
 <main class="min-h-screen bg-[#f7fafc] dark:bg-[#18181c] flex flex-col items-center font-sans pt-[90px]">
-  <div class="w-full max-w-4xl mx-auto px-4">
+  <div class="w-full max-w-4xl mx-auto px-4 text-gray-900 dark:text-gray-100">
     <h1 class="text-4xl font-bold pb-4">F.A.Q.</h1>
   </div>
 </main>
